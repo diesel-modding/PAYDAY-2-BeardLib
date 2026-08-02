e085ac3f66cb6f5ba6b4e4b80e90638c390d694a
Luffy <luffydafloffi@gmail.com>
2026-08-03 00:41:13 +0300

    Remove AssetLoader wren and use dyn resource load instead (not tested)

--- Classes/Managers/FileManager.lua
+++ Classes/Managers/FileManager.lua
@@ -147,7 +147,7 @@ function BeardLibFileManager:LoadFileFromDB(ext, path)
 
 	local k_ext = ext:key()
 
-	blt.wren_io.invoke(BeardLib.Utils:GetNameFromModPath(BeardLib.ModPath), "AssetLoader", nil, "load", ext, path)
+	managers.dyn_resource:load(ext, path, DynamicResourceManager.DYN_RESOURCES_PACKAGE);
     Global.fm.added_files[k_ext] = Global.fm.added_files[k_ext] or {}
 	Global.fm.added_files[k_ext][path:key()] = {path = path}
 
