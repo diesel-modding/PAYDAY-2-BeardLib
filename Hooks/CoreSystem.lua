c60c1aa4db428f2babf93e5710a18b609470110d
Luffy <luffydafloffi@gmail.com>
2026-09-17 17:45:56 +0300

    Disable unit_data overwrite for now
    Fixes #836
    If it causes issues, we may need to think of a way to fix it. Preferably best solution is if the game just didn't crash from trying to read unit_data of unloaded units

--- Hooks/CoreSystem.lua
+++ Hooks/CoreSystem.lua
@@ -55,15 +55,18 @@ overwrite_meta_function(MassUnitManager, "load", function(self, path, ...)
 	return self:_load(path, ...)
 end)
 
-overwrite_meta_function(PackageManager, "unit_data", function(self, unit_name, ...)
-	if unit_name and Global.fm.added_files[key_unit] then
-		local file = Global.fm.added_files[key_unit][tostring(unit_name:key())]
-		if file then
-			Managers.File:LoadAsset(ids_unit, unit_name, file.file)
-		end
-	end
-	return self:_unit_data(unit_name, ...)
+-- Disabled for now, this crashes when overriding existing units, probably related to <dependencies> node on the unit
+-- @HW12 if you can make this just work without needing this dumb workaround it would be really good
+--[[overwrite_meta_function(PackageManager, "unit_data", function(self, unit_name, ...)
+-- 	if unit_name and Global.fm.added_files[key_unit] then
+-- 		local file = Global.fm.added_files[key_unit][tostring(unit_name:key())]
+-- 		if file then
+-- 			Managers.File:LoadAsset(ids_unit, unit_name, file.file)
+-- 		end
+-- 	end
+-- 	return self:_unit_data(unit_name, ...)
 end)
+--]]
 
 overwrite_meta_function(PackageManager, "script_data", function(self, ext, path, name_mt)
 	return Managers.File:Process(ext, path, name_mt)
